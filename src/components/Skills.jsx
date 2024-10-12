7a2d07c503c4edc184145a719f157a460a297cda
HatayErincErdemli <hatayerinc@gmail.com>
2024-10-12 21:59:31 +0300

    language state yönetimi öğrenildi (sağ olun Gökhan hocam)

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import { languages } from "../../data";
+import { MyContext } from "../contexts/Context";
 
 function Skills() {
+  const { langData } = useContext(MyContext);
   return (
     <div className="w-auto h-auto dark:bg-[#242128] px-60 pt-20 pb-10 items-start flex flex-wrap flex-row ">
       <h2 className="text-4xl text-[#4731d2] dark:text-[#cbf180] font-bold mr-20">
-        {languages.en.skills.title}
+        {langData.skills.title}
       </h2>
       <div className="flex flex-row flex-wrap justify-between w-3/4">
-        {languages.en.skills.skills_items.map(({ logo, name }, index) => {
+        {langData.skills.skills_items.map(({ logo, name }, index) => {
           return (
             <div
               className="skills-items flex flex-row items-center justify-between space-x-4 px-28 pb-10 "
--- src/contexts/Context.jsx
+++ src/contexts/Context.jsx
@@ -1,29 +1,32 @@
 import { createContext, useEffect, useState } from "react";
-
+import { languages } from "../../data";
+import axios from "axios";
 export const MyContext = createContext();
 
 export const MyContextProvider = ({ children }) => {
   const [darkMode, setDarkMode] = useState(false);
   const [lang, setLang] = useState("tr");
-  const [langData, setLangData] = useState({});
-  /*useEffect(() => {
+  const [langData, setLangData] = useState(() => {
+    return languages["tr"];
+  });
+  useEffect(() => {
     fetchLanguageData();
-  }, []);
+  }, [lang]);
   console.log(langData);
 
   const fetchLanguageData = () => {
-    const dataUrl = `/${lang}.js`;
-    axios.get(dataUrl).then((res) => {
+    const dataUrl = `https://reqres.in/api/workintech`;
+    axios.post(dataUrl, languages[lang]).then((res) => {
       setLangData(res.data);
     });
-  };*/
+  };
 
   function toggleDarkMode() {
     setDarkMode(!darkMode);
     document.body.classList.toggle("dark");
   }
   return (
-    <MyContext.Provider value={{ toggleDarkMode, darkMode }}>
+    <MyContext.Provider value={{ toggleDarkMode, darkMode, langData }}>
       {children}
     </MyContext.Provider>
   );
